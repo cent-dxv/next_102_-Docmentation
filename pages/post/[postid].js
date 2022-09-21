f41e1938d341a5eedb827f1dcd54c1fe54a73887
cent-dxv <centdxv@gmail.com>
2022-09-21 17:03:58 +0300

    fall back true

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -1,13 +1,19 @@
 import { useRouter } from "next/router";
 
-function Post_d({ post ,user}) {
+function Post_d({ post, user }) {
+
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
   return (
     <div>
       <h1>post</h1>
 
-      <h2> {user.name}</h2>
-      <h3> {post.title}</h3>
-      <p> {post.body}</p>
+       <h2> {user.name}</h2> 
+       <h3> {post.title}</h3>
+      <p> {post.body}</p> 
     </div>
   );
 }
@@ -17,44 +23,65 @@ export default Post_d;
 export async function getStaticPaths() {
   const responce = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const data = await responce.json();
-  const paths = data.map((post) => ({
-    // params: { postid: post.id.toString() },
-    params: { postid: `${post.id}` },
-  }));
 
-  return {
-    paths: paths,
-    fallback: false,
-  };
+  // // this fore fall back to fallse and dynamically fill
+  // // fill all paht
+  // const paths = data.map((post) => ({
+  //   // params: { postid: post.id.toString() },
+  //   params: { postid: `${post.id}` },
+  // }));
+
   // return {
-  //   paths: [
-  //     {
-  //       params: { postid: '1' },
-  //     },
-  //     {
-  //       params: { postid: '2' },
-  //     },
-  //   ],
+  //   paths: paths,
   //   fallback: false,
   // };
+
+  // this for fall back true
+  // the path not generated durign build time
+  // will not result 404 page instade it
+  // call for api in back ground and serve while it done
+
+  return {
+    paths: [
+      {
+        params: { postid: "1" },
+      },
+      {
+        params: { postid: "2" },
+      },
+    ],
+    fallback: true,
+  };
 }
 
 export async function getStaticProps(context) {
   const { params } = context;
+
   const responce = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.postid}`
   );
   const data = await responce.json();
+    console.log(data)
+  if (!data.id) {
+    return {
+      notFound: true
+    }
+  }
 
   const user_responce = await fetch(
     `https://jsonplaceholder.typicode.com/users/${data.userId}`
   );
   const user_data = await user_responce.json();
 
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: {
       post: data,
-      user: user_data
+      user: user_data,
     },
   };
 }
