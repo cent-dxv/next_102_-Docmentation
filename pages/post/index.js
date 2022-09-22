e71b59c2907f1b42cef7f31ea0425bfe3c859ece
cent-dxv <centdxv@gmail.com>
2022-09-22 14:23:02 +0300

    pre rendering  from  self API

---
+++ pages/post/index.js
@@ -0,0 +1,30 @@
+import { getStaticProps } from "./[id]"
+
+function index({post}) {
+  return (
+    <div>
+    <h1> calling on data from oun API</h1>
+    {
+      post.map(post => {
+        return (
+          <div key={post.id}>
+            <h2> {post.id}</h2>
+            <h2> {post.name}</h2>
+          </div>
+        )
+      })
+    }
+    
+    
+    </div>
+  )
+}
+
+export default index
+
+export async function getServerSideProps() {
+  const responce = await fetch("http://localhost:3000/api/post");
+  const data = await responce.json();
+
+  return { props: { post: data } };
+}
