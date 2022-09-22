fb4e0b75566948fa29ea5a1c27b6e001b03db52e
cent-dxv <centdxv@gmail.com>
2022-09-22 09:15:47 +0300

    some if needed

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -1,60 +1,32 @@
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
-function Post_d({ post, user }) {
 
-  const router = useRouter()
+const params = useRouter()
+const fetch = async () => {
+  const responce = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postid}`
+  );
+  const data = await responce.json();
+  return data;
+};
 
-  if (router.isFallback) {
-    return <div>Loading...</div>
-  }
+function Post_d() {
+
+  const router = useRouter();
+  const { post, erro } = useSWR('post', fetch);
+  if (erro) return "somting went wring";
+  if(!post) return "loadding"
+
   return (
     <div>
-      <h1>post</h1>
+      <h1>post Swr fetcher</h1>
 
-       <h2> {user.name}</h2> 
-       <h3> {post.title}</h3>
-      <p> {post.body}</p> 
+      {/* <h2> {user.name}</h2> */}
+      <h3> {post.title}</h3>
+      <p> {post.body}</p>
     </div>
   );
 }
 
 export default Post_d;
-
-
-
-export async function getServerSideProps(context) {
-  const { params, req, res } = context;
-  console.log(req.headers.cookies)
-  // res.serHeaders('Set-Cookie',['name = cent' ])
-  // http://localhost:4000/user
-  //   http://localhost:4000/post
-
-  const responce = await fetch(
-    // `http://localhost:4000/post/${params.postid}`
-    `https://jsonplaceholder.typicode.com/posts/${params.postid}`
-  );
-  const data = await responce.json();
-    console.log("server side rendering")
-  if (!data.id) {
-    
-    return {
-      notFound: true
-    }
-  }
-
-  const user_responce = await fetch(
-    // `http://localhost:4000/user/${data.userId}`
-   `https://jsonplaceholder.typicode.com/users/${data.userId}`
-
-    );
-  const user_data = await user_responce.json();
-
- 
-  
-  return {
-    props: {
-      post: data,
-      user: user_data,
-    }
-  };
-}
